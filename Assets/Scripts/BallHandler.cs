8687f11bbc9962b04617c1ed53996238f7557571
Chit Moe Wai <chitmoewai135@gmail.com>
2023-01-31 22:32:57 +0630

    chitmoe : Make a new circle

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class BallHandler : MonoBehaviour
 {
+    public static float rotationSpeed = 75f;
+
     public static Color oneColor = Color.green;
     public GameObject ballPrefab;
 
     private float speed =100;
 
-    
+    private void Start()
+    {
+        MakeNewCircle();
+    }
     void Update()
     {
         if(Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
@@ -23,5 +28,11 @@ public class BallHandler : MonoBehaviour
         GameObject gameObject = Instantiate(ballPrefab, new Vector3(0, 0, -8f), Quaternion.identity);
         gameObject.GetComponent<MeshRenderer>().material.color = oneColor;
         gameObject.GetComponent<Rigidbody>().AddForce(Vector3.forward * speed, ForceMode.Impulse);
+    }
+    void MakeNewCircle()
+    {
+        GameObject gameObject2 = Instantiate(Resources.Load("round" + Random.Range(1, 4))) as GameObject;
+        gameObject2.transform.position = new Vector3(0, 20, 23);
+        gameObject2.name = "Circle";
     }
 }
